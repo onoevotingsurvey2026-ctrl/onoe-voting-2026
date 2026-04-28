9e810ee9c5903bde655f49e56f725ee9f13cb013
onoevotingsurvey2026-ctrl <onoe.voting.survey2026@gmail.com>
2026-04-28 19:21:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let support = 0;
+let against = 0;
+
+function vote(type) {
+  if (type === "support") support++;
+  if (type === "against") against++;
+
+  document.getElementById("result").innerText =
+    "Support: " + support + " | Against: " + against;
+}
