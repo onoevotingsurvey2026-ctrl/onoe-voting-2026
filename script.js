fc47fa22e1e317511b52d9d425ab4f8bb2546ab1
onoevotingsurvey2026-ctrl <onoe.voting.survey2026@gmail.com>
2026-04-28 22:13:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let support = 0;
 let against = 0;
+let voted = false;
 
 function vote(type) {
+
+  if (voted) {
+    alert("You already voted!");
+    return;
+  }
+
+  voted = true;
+
   if (type === "support") support++;
   if (type === "against") against++;
 
+  let total = support + against;
+
   document.getElementById("result").innerText =
     "Support: " + support + " | Against: " + against;
-}
 
-// USER NAME (safe check important)
-window.onload = function () {
-  document.getElementById("user").innerText = "User: N RAMAN";
-};
+  let supPer = ((support / total) * 100).toFixed(1);
+  let agPer = ((against / total) * 100).toFixed(1);
+
+  document.getElementById("percent").innerText =
+    "Support: " + supPer + "% | Against: " + agPer + "%";
+}
