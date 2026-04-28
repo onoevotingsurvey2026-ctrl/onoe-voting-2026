5cbb19b805d841c1a6214e9881ae6eb913f06eec
onoevotingsurvey2026-ctrl <onoe.voting.survey2026@gmail.com>
2026-04-28 20:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-let support = 0;
-let against = 0;
-
-function vote(type) {
-  if (type === "support") support++;
-  if (type === "against") against++;
-
-  document.getElementById("result").innerText =
-    "Support: " + support + " | Against: " + against;
-}
+document.getElementById("user").innerText = "User: N RAMAN";
